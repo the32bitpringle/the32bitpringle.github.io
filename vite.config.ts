759941b785951cf892eb5458282c78a536ace65b
the32bitpringle <ihonestlydontcareifyouhackme@gmail.com>
2026-06-11 11:31:02 -0700

    Configure GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/celere-2/',
   server: {
     proxy: {
       '/api': 'http://localhost:8787',
